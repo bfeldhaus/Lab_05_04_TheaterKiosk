9d5b496428c22b13929e3a6228c8e561a674eb92
brady feldhaus <feldhabl@mail.uc.edu>
2022-02-09 14:01:06 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int age = 23;
+        int drinkingAge = 21;
+
+        if (age >= drinkingAge)
+        {
+            System.out.println("You will receive a wristband");
+        }
+
+    }
+}
